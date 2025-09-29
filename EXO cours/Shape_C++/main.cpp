569501b260c8642d2cb9b634b67bc0b444331f88
Lohan DARQUES <lohan.darques@student.junia.com>
2025-09-29 12:16:21 +0200

    Exo 3,4

--- EXO cours/Shape_C++/main.cpp
+++ EXO cours/Shape_C++/main.cpp
@@ -38,5 +38,36 @@ int main()
     }
     delete[] tab_shape;
 
+    Point2D A(1, 2), B(4, 6);
+    Vecteur v1(A, B);
+
+    Vecteur v2(3, -1);
+    Vecteur v3 = v1 + v2;
+    Vecteur v4 = v3 * 2.0f;
+
+    cout << "v1 = "; v1.print(); cout << endl;
+    cout << "v2 = "; v2.print(); cout << endl;
+    cout << "v3 = "; v3.print(); cout << endl;
+    cout << "v4 = "; v4.print(); cout << endl;
+
+    std::cout << "v1 == v2 ? " << (v1 == v2 ? "oui" : "non") << std::endl;
+
+    vector<Point2D> triangle = { {0,0}, {4,0}, {0,3} };
+    Polygon polyTriangle(triangle);
+    polyTriangle.print();
+    cout << "Aire du triangle = " << polyTriangle.area() << endl;
+
+    // Carré
+    vector<Point2D> carre = { {0,0}, {0,2}, {2,2}, {2,0} };
+    Polygon polyCarre(carre);
+    polyCarre.print();
+    cout << "Aire du carre = " << polyCarre.area() << endl;
+
+    // Rectangle
+    vector<Point2D> rectangle = { {0,0}, {0,3}, {5,3}, {5,0} };
+    Polygon polyRectangle(rectangle);
+    polyRectangle.print();
+    cout << "Aire du rectangle = " << polyRectangle.area() << endl;
+
     return EXIT_SUCCESS;
 }
--- EXO cours/Shape_C++/shape.hpp
+++ EXO cours/Shape_C++/shape.hpp
@@ -1,19 +1,11 @@
 #pragma once
 
+#include <vector>
 #include <iostream>
+#include <cmath>   // pour std::fabs
 
-using std::cout;
-using std::endl;
+using namespace std;
 
-class Vecteur {
-    private :
-        Point2D p1;
-        Point2D p2;
-
-    public:    
-
-
-};
 
 class Point2D {
 private:
@@ -33,6 +25,78 @@ public:
     }
 };
 
+class Vecteur {
+private:
+    float x_;
+    float y_;
+
+public:
+    // --- Constructeurs ---
+    Vecteur(float x = 0.f, float y = 0.f) : x_(x), y_(y) {}
+
+    // Construire un vecteur à partir de 2 points
+    Vecteur(const Point2D& A, const Point2D& B) {
+        x_ = B.get_x() - A.get_x();
+        y_ = B.get_y() - A.get_y();
+    }
+
+    // --- Getters / Setters ---
+    float get_x() const { return x_; }
+    float get_y() const { return y_; }
+    void set_x(float x) { x_ = x; }
+    void set_y(float y) { y_ = y; }
+
+    // --- Opérations ---
+    Vecteur operator+(const Vecteur& v) const {
+        return Vecteur(x_ + v.x_, y_ + v.y_);
+    }
+
+    Vecteur operator*(float k) const {
+        return Vecteur(x_ * k, y_ * k);
+    }
+
+    bool operator==(const Vecteur& v) const {
+        return (std::fabs(x_ - v.x_) < 1e-6 &&
+            std::fabs(y_ - v.y_) < 1e-6);
+    }
+
+    void print() const {
+        std::cout << "(" << x_ << ", " << y_ << ")";
+    }
+};
+
+class Polygon {
+private:
+    vector<Point2D> points_;
+
+public:
+    // Constructeur
+    Polygon(const vector<Point2D>& pts) : points_(pts) {}
+
+    // Calcul de l'aire (formule de Shoelace)
+    float area() const {
+        int n = points_.size();
+        if (n < 3) return 0.f; // pas un polygone
+
+        float sum = 0.f;
+        for (int i = 0; i < n; i++) {
+            const Point2D& p1 = points_[i];
+            const Point2D& p2 = points_[(i + 1) % n]; // relie au premier
+            sum += (p1.get_x() * p2.get_y()) - (p2.get_x() * p1.get_y());
+        }
+        return fabs(sum) / 2.f;
+    }
+
+    // Affichage
+    void print() const {
+        cout << "Polygon: ";
+        for (const auto& p : points_) {
+            p.print();
+            cout << " ";
+        }
+        cout << endl;
+    }
+};
 enum class Color : unsigned char
 {
     blue = 0,
@@ -49,7 +113,7 @@ private:
     Color color_;
 
 public:
-    Shape(const Color &color = Color::green);
+    Shape(const Color& color = Color::green);
     virtual float get_aera() = 0;
     virtual void print_data() = 0;
 };
@@ -61,7 +125,7 @@ protected:
 public:
     float get_aera();
     void print_data();
-    Rectangle(const Point2D &p1, const Point2D &p2);
+    Rectangle(const Point2D& p1, const Point2D& p2);
 };
 
 class Triangle : public Shape
@@ -71,11 +135,11 @@ private:
 public:
     float get_aera();
     void print_data();
-    Triangle(const Point2D &p1, const Point2D &p2,
-        const Point2D &p3);
+    Triangle(const Point2D& p1, const Point2D& p2,
+        const Point2D& p3);
 };
 
-class Circle : public  Shape
+class Circle : public Shape
 {
 private:
     Point2D center;
@@ -84,11 +148,11 @@ private:
 
 public:
     virtual void print_data();
-    Circle(const Point2D &center, const float &radius);
+    Circle(const Point2D& center, const float& radius);
 };
 
-class Square:Rectangle {
+class Square : public Rectangle {
 public:
-    Square(const Point2D &p1, const float &side);
+    Square(const Point2D& p1, const float& side);
     void print_data();
 };
